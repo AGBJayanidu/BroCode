a7819e6eb8979d2b997e7577e26ae91db3e5595f
WGHNirmani <hirunikanirmani26@gmail.com>
2024-02-11 03:45:24 +0530

    contnue from 1.07.29

--- index.js
+++ index.js
@@ -217,23 +217,58 @@
 
 // ############################## counter program ###################################
 
-const decreaseBtn = document.getElementById('decreaseBtn');
-const resetBtn = document.getElementById('resetBtn');
-const increaseBtn = document.getElementById('increaseBtn');
-const countLabel = document.getElementById('countLabel');
-let count = 0;
+// const decreaseBtn = document.getElementById('decreaseBtn');
+// const resetBtn = document.getElementById('resetBtn');
+// const increaseBtn = document.getElementById('increaseBtn');
+// const countLabel = document.getElementById('countLabel');
+// let count = 0;
 
-increaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count;
-}
+// increaseBtn.onclick = function(){
+//     count++;
+//     countLabel.textContent = count;
+// }
 
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
-resetBtn.onclick = function() {
-    count = 0;
-    countLabel.textContent = count;
-}
+// decreaseBtn.onclick = function(){
+//     count--;
+//     countLabel.textContent = count;
+// }
+// resetBtn.onclick = function() {
+//     count = 0;
+//     countLabel.textContent = count;
+// }
 // ############################## Math ###################################
+// Math = built-in object that provides a collection of properties and methods
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// let x = 10;
+// let x = 3.99;
+// let x = 3;
+// let x = 10;
+// let x = 45;
+let x = 3;
+let y = 2;
+let z = 1;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(x, y);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+
+// console.log(z);
+
+let max = Math.max(x,y,z);
+let min = Math.min(x,y, z);
+
+console.log(max);
+console.log(min);
+
